faf890c21b1338554e259fb10a59d317335567bc
MohammadParsa7 <mmd.parsa7@gmail.com>
2026-08-11 23:05:41 +0330

    Update project

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,8 @@ const closeBtn = document.querySelector(".close-btn");
 const btn = document.querySelector(".btn-upload");
 const listBody = document.querySelector(".list-body");
 const inputSearch = document.querySelector(".search");
-const sortBtn = document.querySelector("#sortDate");
-const iconBtn = document.querySelector(".iconPrice");
+const selectSortPrice = document.querySelector(".sort-price");
+const selectSortData = document.querySelector(".sort-data");
 
 // ! EVEANTS
 btn.addEventListener("click", getUser);
@@ -70,27 +70,27 @@ function addData(data) {
            <th>${data.type}</th>
            <th>${data.price}</th>
            <th>${data.refId}</th>
-           <th>${new Date().toLocaleDateString("fa", {
-             year: "numeric",
-             month: "2-digit",
-             day: "2-digit",
-             hour: "2-digit",
-             minute: "2-digit",
-             second: "2-digit",
-           })}</th>
+           <th>
+            ${new Date(data.date).toLocaleString("fa-IR", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+           </th>
            </tr>
            `;
   });
   listBody.innerHTML = result;
 }
 
-// SORT DATA
-const selectSortElement = document.querySelector(".sort-price");
+// SORT PRICE
 
-selectSortElement.addEventListener("change", sortData);
+selectSortPrice.addEventListener("change", sortPrice);
 
-async function sortData() {
-  const selectSort = selectSortElement.value;
+async function sortPrice() {
+  const selectSort = selectSortPrice.value;
 
   const res = await app.get("transactions");
   const data = res.data;
@@ -112,7 +112,6 @@ async function sortData() {
   }
 }
 
-
 // SEARCH DATA
 inputSearch.addEventListener("input", async (e) => {
   const query = e.target.value;
@@ -125,3 +124,28 @@ inputSearch.addEventListener("input", async (e) => {
   addData(filtered);
 });
 
+// SORT DATA
+
+selectSortData.addEventListener("change", sortData);
+
+async function sortData() {
+  const statusSort = selectSortData.value;
+  const { data } = await app.get("transactions");
+  const newest = [...data].sort((a, b) => {
+    return b.date - a.date;
+  });
+  const oldest = [...data].sort((a, b) => {
+    return a.date - b.date;
+  });
+  switch (statusSort) {
+    case "All":
+      return addData(data);
+    case "Newest":
+      return addData(newest);
+    case "Oldest":
+      return addData(oldest);
+
+    default:
+      throw new Error("Please select an option.");
+  }
+}
